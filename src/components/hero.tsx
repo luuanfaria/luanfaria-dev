245ddcd1ec56363a566c9fbce3eb503ff1060ab3
Luan Faria <luuan.fariaf@gmail.com>
2024-07-09 23:25:48 -0300

    Add `Command` and `Tabs`

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,22 @@
 'use client'
 import { useLanguage } from '@/context/languageProvider'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
+
+import { Calendar, CreditCard, Settings, Smile, User } from 'lucide-react'
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command'
+import { PaperPlaneTilt } from '@phosphor-icons/react/dist/ssr'
 
 const dictionary = {
   en: {
@@ -12,6 +26,10 @@ const dictionary = {
       'I am Luan Faria, a Web Designer with over 6 years of experience crafting digital solutions that empower businesses globally. Specializing in user-centric design and strategic optimization. My commitment to excellence has earned me a reputation as a trusted partner in the online business community.',
     press: 'Press',
     pressText: 'anytime to get in touch.',
+    placeholder: 'Type a command or search...',
+    suggestions: 'Suggestions',
+    email: 'Send email to hello@luandev.com.br',
+    book: 'Book a Call',
     button: 'Get in touch',
   },
   pt: {
@@ -21,26 +39,29 @@ const dictionary = {
       'Eu sou Luan Faria, um Designer Web com mais de 6 anos de experiência desenvolvendo soluções digitais que capacitam negócios globalmente. Especializado em design centrado no usuário e otimização estratégica. Minha dedicação à excelência me rendeu uma reputação como um parceiro confiável na comunidade de negócios online.',
     press: 'Aperte',
     pressText: 'a qualquer momento para entrar em contato.',
+    placeholder: 'Digite um comando ou pesquise...',
+    suggestions: 'Sugestões',
+    email: 'Envie email para hello@luandev.com.br',
+    book: 'Agendar Bate-Papo',
     button: 'Entrar em contato',
   },
 }
 
 export function Hero() {
+  const [open, setOpen] = useState(false)
   const { language } = useLanguage()
   const texts = language === 'en' ? dictionary.en : dictionary.pt
 
   useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 's' || event.key === 'S') {
-        window.location.href = 'mailto:hello@luandev.com.br'
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === 'j' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
       }
     }
 
     document.addEventListener('keydown', handleKeyPress)
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress)
-    }
+    return () => document.removeEventListener('keydown', handleKeyPress)
   }, [])
 
   return (
@@ -49,13 +70,40 @@ export function Hero() {
       <h1 className="text-3xl text-zinc-400">{texts.title2}</h1>
       <p className="text-base text-zinc-400 mt-4">{texts.description}</p>
 
-      <p className="mt-4 text-zinc-600 md:block max-sm:hidden">
+      <p className="text-sm text-muted-foreground mt-4 md:block max-sm:hidden">
         {texts.press}{' '}
-        <span className="border border-zinc-400 rounded-md py-1 px-1.5 secondary text-zinc-300 text-sm">
-          S
-        </span>{' '}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>J
+        </kbd>{' '}
         {texts.pressText}
       </p>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder={texts.placeholder} />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem
+              onSelect={() =>
+                (window.location.href =
+                  'https://calendar.app.google/VMaJYJ5fXEhGfxKA8')
+              }
+            >
+              <Calendar className="mr-2 h-4 w-4" />
+              <span>{texts.book}</span>
+            </CommandItem>
+            <CommandItem
+              onSelect={() =>
+                (window.location.href = 'mailto:hello@luandev.com.br')
+              }
+            >
+              <PaperPlaneTilt className="mr-2 h-4 w-4" />
+              <span>{texts.email}</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
 
       <Link
         href="mailto:hello@luandev.com.br"
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,6 +1,7 @@
 'use client'
 import {
   Browser,
+  Code,
   CodeBlock,
   DeviceMobile,
   Globe,
@@ -8,14 +9,18 @@ import {
 } from '@phosphor-icons/react/dist/ssr'
 import { Card, CardContent, CardHeader } from './ui/card'
 import { useLanguage } from '@/context/languageProvider'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 const dictionary = {
   en: {
     title: 'Services I offer',
     description: `Let's work together on your next amazing project.`,
+    byDemand: 'By Demand',
+    daas: 'Developer as a Service',
     services: [
       {
         title: 'Website Landing Page',
+        category: 'byDemand',
         price: 'From $799',
         description:
           'How about a personalized page focused on lead conversions and information? A landing page is ideal for this purpose, impacting your clients with concise information and clear calls to action.',
@@ -23,6 +28,7 @@ const dictionary = {
       },
       {
         title: 'Full Website',
+        category: 'byDemand',
         price: 'From $1.599',
         description:
           'Need a website that includes every detail about your business? A full website can include all the information and services your company offers.',
@@ -30,14 +36,24 @@ const dictionary = {
       },
       {
         title: 'Mobile App',
+        category: 'byDemand',
         price: 'From $3.000',
         description:
           'Build customer loyalty with a custom on-demand app tailored to your needs. Our apps are developed and customized for both Android and Apple devices.',
         icon: <DeviceMobile size={22} />,
       },
       {
+        title: 'FullStack Software Engineer',
+        category: 'daas',
+        price: '$37/hour',
+        description:
+          'Looking for an experienced fullstack developer with a proven track record in multidisciplinary teams? I have 6 years of experience working with React, Next.js, Node.js, React Native, and Solidity.',
+        icon: <Code size={22} />,
+      },
+      {
         title: 'Web Maintenance',
-        price: 'From $37/hour',
+        category: 'daas',
+        price: '$37/hour',
         description:
           'Does your website need maintenance? If you already have a website or system that you want to keep updated, we offer support and maintenance services for websites.',
         icon: <Wrench size={22} />,
@@ -47,9 +63,12 @@ const dictionary = {
   pt: {
     title: 'Serviços Prestados',
     description: `Vamos trabalhar juntos em seu próximo projeto.`,
+    byDemand: 'Por Demanda',
+    daas: 'Desenvolvedor como Serviço',
     services: [
       {
         title: 'Landing Page',
+        category: 'byDemand',
         price: 'A partir de R$2.000',
         description:
           'Que tal uma página personalizada focada em conversões de leads e informativa? Uma landing page é ideal para esse caso, impacte seus clientes com informações curtas e ações claras.',
@@ -57,6 +76,7 @@ const dictionary = {
       },
       {
         title: 'Website Completo',
+        category: 'byDemand',
         price: 'A partir de R$4.000',
         description:
           'Precisa de um site com todos os mínimos detalhes da sua empresa? Um site completo é possível incluir todos as informações e serviços da sua empresa.',
@@ -64,14 +84,24 @@ const dictionary = {
       },
       {
         title: 'Aplicativo Mobile',
+        category: 'byDemand',
         price: 'A partir de R$15.000',
         description:
           'Fidelize seus clientes com um aplicativo sob-demanda para suas necessidades. Nossos aplicativos são desenvolvidos e personalizados para Android e Apple.',
         icon: <DeviceMobile size={22} />,
       },
       {
+        title: 'Desenvolvedor FullStack',
+        category: 'daas',
+        price: 'R$94/hora',
+        description:
+          'Precisa de um profissional experiente em desenvolvimento fullstack, com atuações em times multidisciplinares? Possuo 6 anos de experiência com React, Next, Node, React Native e Solidity.',
+        icon: <Code size={22} />,
+      },
+      {
         title: 'Manutenção Web',
-        price: 'A partir de R$94/hora',
+        category: 'daas',
+        price: 'R$94/hora',
         description:
           'Seu site precisa de manutenção? Já possui um site ou sistema que deseja manter, mas precisa atualizar, oferecemos suporte e manutenção de websites.',
         icon: <Wrench size={22} />,
@@ -92,22 +122,62 @@ export function Services() {
         <p className="text-zinc-400 text-sm">{texts.description}</p>
       </div>
 
-      <div className="flex flex-col w-full gap-4 mt-8">
-        {texts.services.map((item, index) => (
-          <Card key={index}>
-            <CardHeader>{item.icon}</CardHeader>
-            <CardContent>
-              <div className="flex gap-2 text-xl items-center">
-                <h2 className="text-xl">{item.title}</h2>
-                <span className="bg-secondary p-0.5 rounded-sm text-xs text-zinc-800 dark:text-zinc-300 border">
-                  {item.price}
-                </span>
-              </div>
-              <p className="text-sm text-zinc-400 mt-1">{item.description}</p>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+      <Tabs defaultValue="byDemand" className="w-full mt-8">
+        <TabsList>
+          <TabsTrigger value="byDemand">{texts.byDemand}</TabsTrigger>
+          <TabsTrigger value="daas">{texts.daas}</TabsTrigger>
+        </TabsList>
+        <TabsContent value="byDemand">
+          <div className="flex flex-col w-full gap-4 mt-8">
+            {texts.services.map((item, index) => {
+              if (item.category === 'byDemand') {
+                return (
+                  <Card key={index}>
+                    <CardHeader>{item.icon}</CardHeader>
+                    <CardContent>
+                      <div className="flex gap-2 text-xl items-center">
+                        <h2 className="text-xl">{item.title}</h2>
+                        <span className="bg-secondary p-0.5 rounded-sm text-xs text-zinc-800 dark:text-zinc-300 border">
+                          {item.price}
+                        </span>
+                      </div>
+                      <p className="text-sm text-zinc-400 mt-1">
+                        {item.description}
+                      </p>
+                    </CardContent>
+                  </Card>
+                )
+              }
+              return null
+            })}
+          </div>
+        </TabsContent>
+        <TabsContent value="daas">
+          <div className="flex flex-col w-full gap-4 mt-8">
+            {texts.services.map((item, index) => {
+              if (item.category === 'daas') {
+                return (
+                  <Card key={index}>
+                    <CardHeader>{item.icon}</CardHeader>
+                    <CardContent>
+                      <div className="flex gap-2 text-xl items-center">
+                        <h2 className="text-xl">{item.title}</h2>
+                        <span className="bg-secondary p-0.5 rounded-sm text-xs text-zinc-800 dark:text-zinc-300 border">
+                          {item.price}
+                        </span>
+                      </div>
+                      <p className="text-sm text-zinc-400 mt-1">
+                        {item.description}
+                      </p>
+                    </CardContent>
+                  </Card>
+                )
+              }
+              return null
+            })}
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
